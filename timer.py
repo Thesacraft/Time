d976d5d5f5f99c1ad581d04976f4d6c3d9f81af8
Thesacraft <91694323+Thesacraft@users.noreply.github.com>
2022-04-27 21:05:26 +0200

    logging and updatet the timer.py

--- timer.py
+++ timer.py
@@ -12,7 +12,9 @@ description: showing how much time you have left when using a speedport(Telekom)
 
 #Importing
 from selenium import webdriver
+import logging
 from time import sleep
+import sys
 from selenium.webdriver.firefox.options import Options
 #from selenium.webdriver.support.wait import WebDriverWait
 import os
@@ -26,8 +28,18 @@ destroyed: int = 1
 global systray
 global menu_options
 global say_hello
-
 #Quitting the programm correctly
+
+if(len(sys.argv)>1):
+    loglevel = sys.argv[1]
+    if "--log=" in loglevel:
+        loglevel = loglevel.replace("--log=","")
+        numeric_level = getattr(logging,loglevel.upper(),None)
+if(numeric_level!= None):
+    logging.basicConfig(filename="logfile-timer.log", format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',datefmt='%m/%d/%Y %I:%M:%S %p', level=loglevel)
+else:
+    logging.basicConfig(filename="logfile-timer.log", format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
+logging.debug(f"Start arguments: {sys.argv}")
 def on_quit_callback(systray):
     global destroyed
     destroyed = None
@@ -47,46 +59,57 @@ while True:
     driver.get(my_url)
 
     #Searching for the elements
+    div_maxtime = driver.find_element_by_id('maxtimeNolimit')
+
+    bt_yes_div = driver.find_element_by_id('timeruleTimeYes')
+
     p_element = driver.find_element_by_id('var_remainingtime')
     try:
         p_element2 = driver.find_element_by_id('var_trule_to2')  # var_trule_to1
-    except:
+    except BaseException as err:
+        logging.info(f"Unexpected {err=}, {type(err)=}")
         p_element2 = driver.find_element_by_id('var_trule_to1') #var_trule_to1
     p_element3 = driver.find_element_by_id('var_time') #var_time
     wait: float = 0.3
     sleep(wait)
-
     #converting htmlelements to valid variables
+
     vb: float = p_element.text
     bis: float = p_element2.text
     lastupdate: float = p_element3.text
-    
-    #updating the systemtray
-    if(int(vb)>60):
-        vb=int(vb)/60
-        vb=round(vb*10)
-        vb=vb/10
-        systray.update("logo.ico", " Verbleibende Zeit: " + str(vb) + " Stunden und bis " + str(bis) + " Uhr,\n zuletzt geupdatet: " + str(lastupdate) + " Uhr.")
+    if(bt_yes_div.is_displayed()):
+        moeglich="Ja"
     else:
-        systray.update("logo.ico", " Verbleibende Zeit: " + str(vb) + " Minuten und bis " + str(bis) + " Uhr,\n zuletzt geupdatet: " + str(lastupdate) + " Uhr.")
+        moeglich="Nein"
 
+    #updating the systemtray
+    if(not vb == ""):
+        if(int(vb)>60):
+            vb=int(vb)/60
+            vb=round(vb*10)
+            vb=vb/10
+            systray.update("logo.ico", " Verbleibende Zeit: " + str(vb) + " Stunden und bis " + str(bis) + " Uhr,\n Internet Verbindung möglich: {moeglich}\n zuletzt geupdatet: " + str(lastupdate) + " Uhr.")
+        else:
+            systray.update("logo.ico", " Verbleibende Zeit: " + str(vb) + " Minuten und bis " + str(bis) + " Uhr,\n Internet Verbindung möglich: {moeglich}\n zuletzt geupdatet: " + str(lastupdate) + " Uhr.")
+    else:
+        if (div_maxtime.is_enabled()):
+            systray.update("logo.ico", f" Verbleibende Zeit: Unbegränzt\n Internet Verbindung möglich: {moeglich}\n zuletzt geupdatet: " + str(lastupdate) + " Uhr.")
+        else:
+            systray.update("logo.ico",f" Verbleibende Zeit: Abgelaufen\n Internet Verbindung möglich: {moeglich}\n zuletzt geupdatet: " + str(lastupdate) + " Uhr.")
     #closing the driver
     driver.close()
-    os.system('tskill plugin-container')
-    driver.quit()
+    #driver.quit()
     systray.update()
+    logging.debug("looping")
 
     #waiting to the next update and checking if the systemtray was closed during the sleeping
     for x in range(24):
-        if vb != " ":
+        if vb != " " and vb != "" and vb != None:
             if destroyed == None:
-                os.system('taskkill /IM Python')
                 exit()
                 break
             else:
                 sleep(5)
-        else:
-            pass
             
         
         
